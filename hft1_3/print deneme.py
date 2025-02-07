55fc2575023aa6da4725afeac1ab31435c7794ec
dicaprio097 <me0971338@gmail.com>
2025-02-07 20:58:12 +0300

    Add files via upload

---
+++ hft1_3/print deneme.py
@@ -0,0 +1,8 @@
+print("deneme")
+# Diyaz ya da hashtag işareti sonrasında, bulunduğu yerden itibaren
+# satır sonuna kadar açıklama ifadesi olarak kullanılır
+
+"""
+Açıklama ifadeleri üç tek tırnak veya üç çift tırnak arasına
+Çok satırlı olacak şekilde kullanılabilir.
+"""
