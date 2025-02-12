cea5a9f82b6262836db751afa31a26c0f6e5774d
dicaprio097 <me0971338@gmail.com>
2025-02-12 19:43:27 +0300

    if konusu

---
+++ hft2_2/if tekrar.py
@@ -0,0 +1,13 @@
+a = 7
+
+# if True : print("Çalıştı1")
+#if False : print("Çalıştı2")
+
+print(a>5)
+print(a<5)
+
+# if a >3 : print("a 3ten büyük")
+# if a >5 : print("a 5ten büyük")
+
+if a>3 : print("a 3ten büyük")
+elif a>5 :print("a 5ten büyük")
