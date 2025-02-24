85cb810af509b5c08dead94061ebf94c3c8b91b4
dicaprio097 <me0971338@gmail.com>
2025-02-24 21:07:03 +0300

    update

---
+++ hft4_1 sınıf_obje/dizi2 cok boyutlu.py
@@ -0,0 +1,17 @@
+yemek_menusu = [ #iki boyutlu dizi
+    ["tarhana","şehriye","mercimek"]
+    ["musakka","mantı","karnı yarık"]
+    ["tulumba","tiramisu","Lokma tatlısı"]
+]
+
+# print(yemek_menusu)
+# print(yemek_menusu[2])
+print(yemek_menusu[2][1])
+print(["ee","rr","tt"][1])
+
+yemek_menusu = [ #iki boyutlu dizi
+    "corbalar":["tarhana","şehriye","mercimek"]
+    "yemekler":["musakka","mantı","karnı yarık"]
+    "tatlılar":[
+        "tulumba":{100,,"içindekiler":["un","su"]},"tiramisu","Lokma tatlısı"]
+]
